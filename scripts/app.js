240313f23022c050d954df0bf034f1417317e5a0
Brand0nLe <93brandonle@gmail.com>
2023-01-31 20:39:17 -0800

    howler and music update

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,46 @@
+window.music = new Howl({
+  src: ['../assets/DITTODITTO.mp3'],
+  autoplay: false,
+  loop: true,
+  onload: function() {
+    // Check if the audio is currently playing
+    if (localStorage.getItem("musicPlaying") === "true") {
+      this.play();
+      document.getElementById("playPause").innerHTML = "Pause";
 
-const music = document.getElementById("music");
-const playPause = document.getElementById("playPause");
-
-playPause.addEventListener("click", function() {
-  if (music.paused) {
-    music.play();
-    playPause.innerHTML = "Pause";
-  } else {
-    music.pause();
-    playPause.innerHTML = "Play";
+      // Set the current time of the audio to the value stored in the local storage, if it exists
+      const currentTime = localStorage.getItem("musicCurrentTime");
+      if (currentTime) {
+        this.seek(currentTime);
+      }
+    }
   }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const playPauseContainer = document.getElementById("playPauseContainer");
+  if (window.location.pathname !== '/index.html') {
+    playPauseContainer.style.display = 'none';
+  }
+  
+  document.getElementById("playPause").addEventListener("click", function() {
+    if (window.music.playing()) {
+      window.music.pause();
+      this.innerHTML = "Play";
+      localStorage.setItem("musicPlaying", "false");
+    } else {
+      window.music.play();
+      this.innerHTML = "Pause";
+      localStorage.setItem("musicPlaying", "true");
+    }
+  });
+});
+
+// Store the current time of the audio in the local storage whenever it changes
+window.music.on("seek", function() {
+  localStorage.setItem("musicCurrentTime", window.music.seek());
+});
+
+window.music.on("pause", function() {
+  localStorage.setItem("musicCurrentTime", window.music.seek());
 });
